3987bf88a5d3d43fcc917dda499f4117a0da9f72
Tomoyuki Sato <m@sato-t.net>
2020-11-05 17:25:59 +0900

    implements errorneko

--- frontend/app/src/views/ErrorPage.vue
+++ frontend/app/src/views/ErrorPage.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="error-page">
-    にゃーん
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <div class="text-h2 text-center my-4">ERROR</div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-img :src="require('@/assets/errorneko.jpg')" class="mx-auto" max-width="600"/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
