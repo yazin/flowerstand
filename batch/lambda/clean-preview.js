8965ed57d42879c2b15d23f58e288a0a00f507f4
Tomoyuki Sato <m@sato-t.net>
2020-11-29 20:01:43 +0900

    add lambda function to cleanup previews

---
+++ batch/lambda/clean-preview.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+  const params = {
+    Bucket: process.env.S3_BUCKET,
+    Prefix: 'preview/'
+  };
+  const previews = await s3.listObjectsV2(params).promise();
+  const deleteParams = {
+    Bucket: process.env.S3_BUCKET,
+    Delete: {
+      Objects: previews.Contents.map((v) => {
+        return {Key: v.Key}
+        }).filter((v) => {
+          return v.Key !== 'preview/'
+        }),
+      Quiet: true
+    }
+  };
+  await s3.deleteObjects(deleteParams).promise();
+
+  const response = {
+    statusCode: 200,
+  };
+  return response;
+};
