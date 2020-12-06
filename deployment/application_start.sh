b796e3e25da1b5f0732355ee6baa5bcb4cf98dc3
Tomoyuki Sato <m@sato-t.net>
2020-12-06 21:24:22 +0900

    change owner of deployed resources

--- deployment/application_start.sh
+++ deployment/application_start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+sudo chown -R ec2-user:ec2-user $HOME/flowerstand
+sudo chown -R nginx:nginx /var/www/html
 pm2 start flowerstand_server
 sudo systemctl start nginx
