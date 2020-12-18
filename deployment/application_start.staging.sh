9249b48d6b9dec3c01418451ffc630704340f3be
Tomoyuki Sato <m@sato-t.net>
2020-12-19 02:00:14 +0900

    kill and start pm2 on staging deploy

--- deployment/application_start.staging.sh
+++ deployment/application_start.staging.sh
@@ -3,5 +3,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 sudo chown -R ec2-user:ec2-user $HOME/flowerstand
 sudo chown -R nginx:nginx /var/www/html
-pm2 start flowerstand_server
+pm2 start $HOME/pm2.config.js
 sudo systemctl start nginx
--- deployment/before_install.staging.sh
+++ deployment/before_install.staging.sh
@@ -2,4 +2,4 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 sudo systemctl stop nginx
-pm2 stop flowerstand_server
+pm2 kill
